427d8999c62ab6063b87ed3c36b431feb0b0231e
coinmoles <andrew421@gmail.com>
2021-12-24 11:22:59 +0900

    Added status

--- src/events/Client Events/ReadyEvent.ts
+++ src/events/Client Events/ReadyEvent.ts
@@ -1,6 +1,7 @@
 import { RunFunction } from "../../utils/interfaces/Command";
 
 export const run: RunFunction = async (client) => {
-    client.logger.success(`Me is now online!`)
+    client.user?.setActivity("'푸딩아 도움말'을 입력해 주세요!")
+    client.logger.success(`Pudding is now online!`)
 }
 export const name: string = 'ready';
