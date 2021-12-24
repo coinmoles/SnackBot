ae0407eaabf08c36b8d95bdebaf4681eacfc8729
coinmoles <andrew421@gmail.com>
2021-12-24 11:39:05 +0900

    Changed bot name from "푸딩" to "세모"

--- src/events/Client Events/ReadyEvent.ts
+++ src/events/Client Events/ReadyEvent.ts
@@ -1,7 +1,7 @@
 import { RunFunction } from "../../utils/interfaces/Command";
 
 export const run: RunFunction = async (client) => {
-    client.user?.setActivity("'푸딩아 도움말'을 입력해 주세요!")
+    client.user?.setActivity("'세모야 도움말'을 입력해 주세요!")
     client.logger.success(`Pudding is now online!`)
 }
 export const name: string = 'ready';
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const PREFIX = "푸딩아";
+export const PREFIX = "세모야";
 export const SNACK_BACKEND_URL = "https://snack-backend.herokuapp.com"
 export const MEAL_BACKEND_URL = "https://sshs-meal-api-kklmx0m20-hamerin.vercel.app/api"
